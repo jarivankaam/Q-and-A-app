3fcc72d1a03e8dcb185dd86a1f039b7d7e784308
Steven Boeren <70892306+stevenboeren@users.noreply.github.com>
2023-02-28 15:39:15 +0100

    Home links to dashboard

--- resources/views/livewire/header-component.blade.php
+++ resources/views/livewire/header-component.blade.php
@@ -1,15 +1,12 @@
 <div>
     <header class="bg-white py-5 border-t-4 border-black">
         <h1 class="text-center text-black py-4 uppercase font-black">
-            Ask away in this Q And A
+            <a href="/">Ask away in this Q And A</a>
         </h1>
     </header>
     <nav class="bg-white border-t-4  text-black py-2">
         <ul class="flex justify-center">
-            <li class="px-4 py-2"><a href="#">Menu Item 1</a></li>
-            <li class="px-4 py-2"><a href="#">Menu Item 2</a></li>
-            <li class="px-4 py-2"><a href="#">Menu Item 3</a></li>
-            <li class="px-4 py-2"><a href="#">Menu Item 4</a></li>
+            <li class="px-4 py-2"><a href="/dashboard">Inloggen/Registeren</a></li>
         </ul>
     </nav>
 </div>
