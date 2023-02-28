634a5cb8e69cfee8089e335eff2029088a6839f2
jarivankaam <D281531@edu.curio.nl>
2023-02-28 10:02:21 +0100

    added header livewire component

---
+++ resources/views/livewire/header-component.blade.php
@@ -0,0 +1,15 @@
+<div>
+    <header class="bg-white py-5 border-t-4 border-blue-700">
+        <h1 class="text-center text-white py-4 uppercase font-black">
+            Jari van Kaam - Webdeveloper
+        </h1>
+    </header>
+    <nav class="bg-black text-white py-2">
+        <ul class="flex justify-center">
+            <li class="px-4 py-2"><a href="#">Menu Item 1</a></li>
+            <li class="px-4 py-2"><a href="#">Menu Item 2</a></li>
+            <li class="px-4 py-2"><a href="#">Menu Item 3</a></li>
+            <li class="px-4 py-2"><a href="#">Menu Item 4</a></li>
+        </ul>
+    </nav>
+</div>
