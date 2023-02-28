53809be947aae278c459bd0a61a65acd93359acb
jarivankaam <D281531@edu.curio.nl>
2023-02-28 16:13:41 +0100

    opgeriumd jonugh

--- app/Http/Livewire/QAPostsComponent.php
+++ app/Http/Livewire/QAPostsComponent.php
@@ -10,11 +10,9 @@ class QAPostsComponent extends Component
     public function render()
     {
         // return view of livewire component with posts data from database 
-        return view('livewire.q-a-posts-component', ['posts' => Post::all()]);
-
-            //mag ik een kusje?
-
-     
-
+        return view(
+            'livewire.q-a-posts-component', 
+            ['posts' => Post::all()
+        ]);
     }
 }
