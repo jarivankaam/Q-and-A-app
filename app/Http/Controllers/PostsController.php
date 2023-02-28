dbc528eab1dc3a1d3852dab744a9ae2edf7ff71d
jarivankaam <D281531@edu.curio.nl>
2023-02-28 16:11:13 +0100

    frontend done

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    public function index()
+    {
+       //use post model to get all posts
+         $posts = Post::all();
+    }
 }
--- app/Http/Livewire/QAPostsComponent.php
+++ app/Http/Livewire/QAPostsComponent.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class QAPostsComponent extends Component
 {
     public function render()
     {
-        return view('livewire.q-a-posts-component');
+        // return view of livewire component with posts data from database 
+        return view('livewire.q-a-posts-component', ['posts' => Post::all()]);
+
+            //mag ik een kusje?
+
+     
+
     }
 }
