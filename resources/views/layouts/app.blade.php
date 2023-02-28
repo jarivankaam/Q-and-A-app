7026f145b517c33ba2bff52513212d47e1b44026
jarivankaam <D281531@edu.curio.nl>
2023-02-28 16:19:22 +0100

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,11 @@
 
 <body class="font-sans antialiased">
     <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-        @include('layouts.navigation')
+        
+        @if (Auth::check())
+            @include('layouts.navigation')
+        @endif
+
         <!-- Page Heading -->
         @if (isset($header))
             {{ $header }}
