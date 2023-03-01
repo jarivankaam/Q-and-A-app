537bc504add991ecf7725fd13d80a26820b7e612
jarivankaam <D281531@edu.curio.nl>
2023-03-01 14:18:48 +0100

    Query-corner | layouts | kut phpstorm

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
         @endif
 
         @foreach ($posts as $post)
-            <div class="flex flex-col justify-center items-center mt-10" >
+            <div class="flex flex-col justify-center items-center mt-10">
                 <div class="w-1/2">
                     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                         <div class="p-6 text-gray-900 dark:text-gray-100">
