705ed1b96ece7c207d7a6baa64bb742e36393d37
jarivankaam <D281531@edu.curio.nl>
2023-03-01 14:18:02 +0100

    Query-corner | layouts | kut phpstorm

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,22 @@
     </x-slot>
 
     <x-slot name="slot">
-        <p>Here posts will be ar....</p>
+
+        @if(empty($posts))
+            <p>Here posts will be ar....</p>
+        @endif
+
+        @foreach ($posts as $post)
+            <div class="flex flex-col justify-center items-center mt-10" >
+                <div class="w-1/2">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            <h1 class="text-2xl font-bold">{{ $post->title }}</h1>
+                            <p class="text-sm">{{ $post->contents }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </x-slot>
 </x-app-layout>
