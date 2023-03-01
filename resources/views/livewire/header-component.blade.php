69ff828870982f73d487565e4d45b7e869bd8f49
jarivankaam <D281531@edu.curio.nl>
2023-03-01 09:32:29 +0100

    style bitch

--- resources/views/livewire/header-component.blade.php
+++ resources/views/livewire/header-component.blade.php
@@ -1,13 +1,14 @@
 <div>
     <header class="bg-white py-5 border-t-4 border-black">
-        <h1 class="text-center text-black py-4 uppercase font-black">
-            <a href="/">Query Corner: A Place for All Your Inquiries</a>
+        <h1 class="text-center text-black py-4 uppercase">
+            Query Corner: <span> Place for All Your Inquiries</span>
         </h1>
     </header>
     <nav class="bg-white  text-black py-2">
         <ul class="flex justify-center">
             <li class="px-4 py-2"><a href="/dashboard">Inloggen</a></li>
             <li class="px-4 py-2"><a href="/register">Registeren</a></li>
+            <li class="px-4 py-2"><a href="/">Home</a></li>
         </ul>
     </nav>
 </div>          
