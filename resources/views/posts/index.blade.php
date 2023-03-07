c0e899fda2133cbd301b3c7e599739c68e8dee85
jarivankaam <D281531@edu.curio.nl>
2023-03-07 10:38:12 +0100

    Uptree | cosmetic changes | steven is een heel lief iemand

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,24 +7,22 @@
     <table class="mt-8 mr-auto ml-auto w-3/4 divide-y divide-gray-200">
         <thead class="bg-darkblue-50">
         <tr>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Message</th>
-            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">User</th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Message') }}</th>
+            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">{{ __('Message created') }}</th>
         </tr>
         </thead>
         <tbody class=" divide-y divide-gray-200">
         @foreach($posts as $post)
             <tr class="text-left">
-                <td class="px-6 py-4 whitespace-nowrap ">
-                    <a href="#" class="font-medium text-indigo-600 hover:text-indigo-900">{{$post->message}}</a>
-                </td>
-                @foreach($users as $user)
-                    @if($post->user_id == $user->id)
-                        <td class="px-6 py-4 whitespace-nowrap">{{$user->name}}</td>
-                    @else
-                        <td class="px-6 py-4 whitespace-nowrap">{{$post->user_id}}</td>
-                    @endif
-                @endforeach
-
+               <!--- check if the logged in user has the same userid as the post --->
+                @if(Auth::id() == $post->user_id)
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="text-sm text-white-900">{{$post->message}}</div>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="text-sm text-white-900">{{$post->created_at->diffForHumans()}}</div>
+                    </td>
+                @endif
 
             </tr>
         @endforeach
